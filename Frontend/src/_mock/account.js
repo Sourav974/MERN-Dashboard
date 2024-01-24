632411a28c8641ed7aeeb87fdb569ca22f887dff
Parminder Singh <parminder.s@gmail.com>
2024-01-24 14:16:32 +0530

    start uo

---
+++ Frontend/src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Jaydon Frankie',
+  email: 'demo@gmail.com',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+};
+
+export default account;
