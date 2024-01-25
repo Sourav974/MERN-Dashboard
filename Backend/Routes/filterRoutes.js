1e12905bb3684f8e0c918b81553b619bbe544388
Sourav <sourav@Souravs-MBP.domain.name>
2024-01-25 06:19:21 +0530

    added routes

--- Backend/Routes/filterRoutes.js
+++ Backend/Routes/filterRoutes.js
@@ -1,10 +1,17 @@
-// routes/filterRoutes.js
-
 const express = require("express");
 const filterController = require("../Controller/filterController");
 
 const router = express.Router();
 
- 
+router.get("/getAll", filterController.getAllResults);
+router.get("/filterByEndYear", filterController.filterByEndYear);
+router.get("/filterByTopic", filterController.filterByTopic);
+router.get("/filterBySector", filterController.filterBySector);
+router.get("/filterByRegion", filterController.filterByRegion);
+router.get("filterByPest", filterController.filterByPEST);
+router.get("/filterBySource", filterController.filterBySource);
+router.get("/filterBySWOT", filterController.filterBySWOT);
+router.get("/filterByCountry", filterController.filterByCountry);
+router.get("/filterByCity", filterController.filterByCity);
 
 module.exports = router;
